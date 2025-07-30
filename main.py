5d39fac978468735a14858cee56955bc5567f0d0
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-07-30 14:41:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python 3 main.py <path_to_book>")
+        print("Usage: python main.py <path_to_book>")
         sys.exit(1)
     book_path = sys.argv[1]
     print_report(book_path)
